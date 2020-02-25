b1362b2ac82c97d9214687c945052ebc351a6269
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-02-25 15:48:12 -0500

    Set window size based on config

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace MyFirstApp
 {
@@ -6,7 +8,20 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
+            ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            //configBuilder.AddJsonFile("appsettings.json");
+            var inMemoryConfig = new Dictionary<string, string>()
+            {
+                ["AppConfiguration:MainWindow:Width"] = "70",
+                ["AppConfiguration:MainWindow:Height"] = "10"
+            };
+            configBuilder.AddInMemoryCollection(inMemoryConfig);
+            IConfiguration config = configBuilder.Build();
+            //Not supported on Mac https://github.com/dotnet/runtime/issues/27216
+            Console.SetWindowSize(int.Parse(config["AppConfiguration:MainWindow:Width"]), int.Parse(config["AppConfiguration:MainWindow:Height"]));
+            Console.WriteLine($"Width={Console.WindowWidth}, Height={Console.WindowHeight}");
             Console.WriteLine("Hello World!");
+            
         }
     }
 }
